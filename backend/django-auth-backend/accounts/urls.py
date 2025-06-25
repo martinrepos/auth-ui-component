e3e65cc9204e7d1557d5ae3be3899ecc3f71b821
martinrepos <martpeder@icloud.com>
2025-06-25 14:00:13 +0200

    started simplejwt integration

--- backend/django-auth-backend/accounts/urls.py
+++ backend/django-auth-backend/accounts/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from .views import UserListView
 from django.http import JsonResponse
-
+from .views import RegisterView  
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("users/", UserListView.as_view(), name="user-list"),
-    path("", lambda request: JsonResponse({"message": "API root"})),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("", lambda request: JsonResponse({"message": "API root"})), 
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
 ]
--- backend/django-auth-backend/accounts/views.py
+++ backend/django-auth-backend/accounts/views.py
@@ -3,9 +3,23 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import UserSerializer  
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.permissions import IsAuthenticated
 
-class UserListView(APIView):
+class UserListView(APIView): #APIView for users
+    permission_classes = [IsAuthenticated]  
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
+
+class RegisterView(APIView): #APIView for user registration
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "User created successfully"}, status=201)
+        return Response(serializer.errors, status=400)
+
+    
+        
